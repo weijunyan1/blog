5bd7be2e7b25193e8a449337c6fdd84f222248b9
WeiJunYan <1956254031@qq.com>
2020-06-10 23:01:49 +0800

    6-10

---
+++ 2020/2020-06-10/code-02/adminSystem/app.js
@@ -0,0 +1,21 @@
+// 1.导入包
+// 1.1 导入express框架
+const express = require('express');
+// 1.2 导入path模块
+const path = require('path');
+
+// 2.设置包
+// 2.1 使用express方法创建Web服务
+const app = express();
+// 2.2 设置静态资源目录
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// 3.处理请求
+
+
+
+// 4.指定端口启动服务
+app.listen(3000, () => {
+  console.log("服务器已启动,请打开 http://localhost");
+})
