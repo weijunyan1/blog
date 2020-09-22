520863fe22a38ab4458f061f4fc1a6cd30e44058
WeiJunYan <1956254031@qq.com>
2020-09-22 11:43:09 +0800

    9-22

---
+++ 2020/js高级/9-21/08-表单验证/js/reg.js
@@ -0,0 +1,17 @@
+//当页面加载完毕时开始执行
+window.onload=function(){
+    //获取文本框
+    //获取电话栏
+    let tel=document.querySelector('#tel');
+    //获取qq栏
+    let qq=document.querySelector("#qq");
+    //获取昵称栏
+    let nickname=document.querySelector('#nc')
+    //获取短信验证码
+    let  msg=document.querySelector("#msg")
+    //获取登录密码
+    var password=document.querySelector('pwd');
+    //获取输入密码
+    var sure
+
+}
